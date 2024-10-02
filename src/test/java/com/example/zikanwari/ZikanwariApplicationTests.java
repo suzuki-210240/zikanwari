6340a202c6748b30b98dd438a4af68ca2263eb76
Suzuki <tejc210240@gmail.com>
2024-10-02 11:00:00 +0900

    初期設定

---
+++ src/test/java/com/example/zikanwari/ZikanwariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zikanwari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZikanwariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
