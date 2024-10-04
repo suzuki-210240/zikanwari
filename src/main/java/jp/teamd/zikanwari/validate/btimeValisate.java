e34de6879794b81b3639f1fae701c478f4be91c2
Suzuki <tejc210240@gmail.com>
2024-10-04 12:42:56 +0900

    更新

--- src/main/java/jp/teamd/zikanwari/validate/btimeValisate.java
+++ src/main/java/jp/teamd/zikanwari/validate/btimeValisate.java
@@ -2,18 +2,18 @@ package jp.teamd.zikanwari.validate;
 import jakarta.validation.*;
 import jp.teamd.zikanwari.validate.btimeValid.time;
 
-public class btimeValisate implements ConstraintValidator<time,String>{
-    String ok;
+public class btimeValisate implements ConstraintValidator<time,Integer>{
+    //Integer ok;
     @Override
-    public void initialize(time wr){
-        ok = wr.ok();
+    public void initialize(time tm){
+        //ok = tm.ok();
     }
     @Override
-    public boolean isValid(String in,ConstraintValidatorContext cxt){
+    public boolean isValid(Integer in,ConstraintValidatorContext cxt){
         if(in == null){
             return false;
         }
-        System.out.println(in.equals(ok));
-        return in.equals(ok);
+        //System.out.println(in.equals(ok));
+        return in == 31 || in == 32;
     }
 }
